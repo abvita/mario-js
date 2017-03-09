a3d8a3b5f8441a655f1b55693464b21cfffea292
Alexa Vita <alexvita2@gmail.com>
2017-03-09 18:34:32 -0500

    implemented custom height field and button

--- mario.js
+++ mario.js
@@ -12,21 +12,26 @@ function determineHeightAndThenDrawPyramid() {
 
     // TODO 3
     // figure out the height the user typed (replace the "5" below)
-    heightStr = "5";
+    heightStr = document.getElementById("height").value;
 
     // here we convert the string to an int
     height = parseInt(heightStr);
 
     // TODO 2
     // draw the pyramid with the given height
+    drawPyramid(height);
 
-}
-
+};
 
 // TODO 1
 // hook up the button's click event to our determineHeightAndThenDrawPyramid function
+button = document.getElementById("bttn");
 
+button.onclick = function() {
 
+  determineHeightAndThenDrawPyramid();
+
+};
 
 /**
  * drawPyramid
@@ -37,6 +42,7 @@ function determineHeightAndThenDrawPyramid() {
 
      // TODO 4
      // before drawing, clear the old content
+     document.getElementById("pyramid").innerText = "";
 
 
      // for each row....
